34d2ecfe6f0177fd7c532316c675015911f4574a
T-Dex <dex.m.mills@gmail.com>
2017-06-21 08:15:40 -0600

    Add redux and react-dom packages

---
+++ src/navbar/logOut.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class LogOut extends Component {
+  render() {
+    return (
+      
+     )
+  }
+}
+
+export default LogOut
