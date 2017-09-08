7e2074d62ad3f41defb5f305674ecdb0c7a38770
Aaron London <github@postmechanical.com>
2017-09-08 16:28:15 -0700

    Set redirect uri via env var

--- src/Auth.js
+++ src/Auth.js
@@ -5,7 +5,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'wba-data.auth0.com',
     clientID: 'bi2wASzzaMq4Jayq71GpDezJ8nioFFPG',
-    redirectUri: 'http://localhost:3000/callback',
+    redirectUri: process.env.REACT_APP_AUTH_REDIRECT_URI,
     audience: 'https://wba-data.auth0.com/userinfo',
     responseType: 'token id_token',
     scope: 'openid'
