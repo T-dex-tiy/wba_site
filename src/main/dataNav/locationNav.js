c19e8bda9eecb8c10ca9dc134053676011367f59
Aaron London <github@postmechanical.com>
2017-09-10 12:08:51 -0700

    Populate season and trailhead menus

--- src/main/dataNav/locationNav.js
+++ src/main/dataNav/locationNav.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import Dropdown from '../dataNav/dropdownNav.js';
 
 class LocationNav extends Component {
-  constructor(){
+  constructor() {
     super()
+    this.state = { season: null };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleChange(event) {
+    if (event.target.name != "seasons") {
+      return;
+    }
+    var value = event.target.value;
+    this.setState({ season: value });
   }
 
   //if statement goes here for rendering second dropdown select
@@ -14,14 +23,27 @@ class LocationNav extends Component {
 
   render() {
     const {locationInfo} = this.props;
+    var seasons = Object.keys(this.props.locationInfo);
+    var selectedSeason = seasons[0];
+    if (this.state.season != null) {
+      selectedSeason = this.state.season;
+    }
+    var trailheads = Object(this.props.locationInfo[selectedSeason]);
     return (
       <div>
-      <select id="dataSet">
+      Season:
+      <select name="seasons" value={selectedSeason} onChange={this.handleChange}>
       {
-        Object
-        .keys(this.props.locationInfo)
-        .map(key=>{
+        seasons.filter(key => { return key != "key" }).map(key=>{
           return <Dropdown key={key} index={key} locationInfo={Object.keys(locationInfo)}
+          />})
+        }
+      </select>
+      Trailhead:
+      <select name="trailheads" onChange={this.handleChange}>
+      {
+        Object.keys(trailheads).filter(key => { return key != "key" }).map(key=>{
+          return <Dropdown key={key} index={key}
           />})
         }
       </select>
