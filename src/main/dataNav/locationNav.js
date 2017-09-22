37092016a60e9482cb898a31727976426c2604f5
T-Dex <dex.m.mills@gmail.com>
2017-09-21 21:41:58 -0600

    Correct plumbing

--- src/main/dataNav/locationNav.js
+++ src/main/dataNav/locationNav.js
@@ -7,12 +7,11 @@ import Input from '../dataNav/input.js';
 
 class LocationNav extends Component {
   constructor() {
-    super()
+    super();
   }
 
-
   render() {
-    const {locationInfo} = this.props;
+    const { locationInfo } = this.props;
     var seasons = Object.keys(locationInfo);
     var trailheads = [];
     var dates = [];
@@ -25,58 +24,98 @@ class LocationNav extends Component {
     }
 
     if (locationInfo[selectedSeason]) {
-      selectedTrailhead = Object.keys(locationInfo[selectedSeason]).filter(key => { return key !== "key" })[0];
+      selectedTrailhead = Object.keys(
+        locationInfo[selectedSeason]
+      ).filter(key => {
+        return key !== 'key';
+      })[0];
     }
 
     if (this.props.trailhead != null) {
-      selectedTrailhead = this.props.trailhead
+      selectedTrailhead = this.props.trailhead;
     }
 
     if (selectedSeason) {
       trailheads = Object.keys(locationInfo[selectedSeason]);
-      console.log("figuring this out", locationInfo[selectedSeason][selectedTrailhead]);
-      if (locationInfo[selectedSeason][selectedTrailhead]!=null){
+      console.log(
+        'figuring this out',
+        locationInfo[selectedSeason][selectedTrailhead]
+      );
+      if (locationInfo[selectedSeason][selectedTrailhead] != null) {
         if (selectedTrailhead) {
-          Object.keys(locationInfo[selectedSeason][selectedTrailhead]).filter(key => { return key !== "key" }).forEach(key => {
-            dates.push(key);
-          });
+          Object.keys(locationInfo[selectedSeason][selectedTrailhead])
+            .filter(key => {
+              return key !== 'key';
+            })
+            .forEach(key => {
+              dates.push(key);
+            });
+        }
       }
-    }
     }
 
     return (
       <div className="locationData">
         <div className="dateTrailhead">
           Season:
-          <select name="seasons" placeholder="Select a Season" value={selectedSeason} onChange={this.props.handleChangeSeason}>
-          {
-            seasons.filter(key => { return key !== "key" }).map(key=>{
-              return <Dropdown key={key} index={key}
-              />})
-            }
+          <select
+            name="seasons"
+            placeholder="Select a Season"
+            value={selectedSeason}
+            onChange={this.props.handleChangeSeason}
+          >
+            {seasons
+              .filter(key => {
+                return key !== 'key';
+              })
+              .map(key => {
+                return <Dropdown key={key} index={key} />;
+              })}
           </select>
           Trailhead:
-          <select name="trailheads" placeholder="Select a Trailhead" value={this.props.trailhead} onChange={this.props.handleChangeTrailhead}>
-          {
-            trailheads.filter(key => { return key !== "key" }).map(key=>{
-              return <Dropdown key={key} index={key}
-              />})
-            }
+          <select
+            name="trailheads"
+            placeholder="Select a Trailhead"
+            value={this.props.trailhead}
+            onChange={this.props.handleChangeTrailhead}
+          >
+            {trailheads
+              .filter(key => {
+                return key !== 'key';
+              })
+              .map(key => {
+                return <Dropdown key={key} index={key} />;
+              })}
           </select>
           Date:
-          <select name ="dates" placeholder="Select a Date" value={this.props.date} onChange={this.props.handleChangeDay}>
-            {
-              dates.map(key=>{
-                return <DateDropdown key={key} index={key}
-                />})
-              }
+          <select
+            name="dates"
+            placeholder="Select a Date"
+            value={this.props.date}
+            onChange={this.props.handleChangeDay}
+          >
+            {dates.map(key => {
+              return <DateDropdown key={key} index={key} />;
+            })}
           </select>
         </div>
-      <div className="displaydata" ><LocationDisplay seasons={this.props.season} trailhead={this.props.trailhead} day={this.props.date}/></div>
-      <div><Input trailheads={this.props.trailhead} day={this.props.date} updateCountData={this.props.updateCountData}/></div>
+        <div className="displaydata">
+          <LocationDisplay
+            seasons={this.props.season}
+            trailhead={this.props.trailhead}
+            day={this.props.date}
+          />
+        </div>
+        <div>
+          <Input
+            trailhead={this.props.trailhead}
+            day={this.props.date}
+            updateCountData={this.props.updateCountData}
+          />
+        </div>
       </div>
-     )
+    );
   }
 }
 
-export default LocationNav
+export default LocationNav;
